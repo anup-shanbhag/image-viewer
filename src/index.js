713b87bf8f4ecb37037ed10ea610b283b49eec1d
anup-shanbhag <anupshanbhag.sd11@iiitb.net>
2020-08-06 23:12:56 +0200

    Moved header component to common/header/ (from /common/)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './common/Header';
-import './common/Header.css'
+import Header from './common/header/Header';
+import './common/header/Header.css'
 
 ReactDOM.render(
     <Header />, document.getElementById('root')
