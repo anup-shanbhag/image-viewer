51f1a8fcddd3f572b2c344a6a0ae86474dd19568
anup-shanbhag <anupshanbhag.sd11@iiitb.net>
2020-08-09 02:31:30 +0200

    Added UI for Home Page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,15 +3,36 @@ import Header from '../../common/header/Header';
 import Search from '../../common/search/Search';
 import Profile from '../../common/profile/Profile';
 import './Home.css';
+import { Box, Card } from '@material-ui/core';
+import { postDetail, posts, postsDetail } from '../../common/Test';
+import PostHeader from '../../common/post/PostHeader';
+import PostFooter from '../../common/post/PostFooter';
+import PostContent from '../../common/post/PostContent';
 
 export default class Home extends Component {
     constructor() {
         super();
+        /*this.postData = posts.data.map(post=>{
+            let postDetails = postsDetail.find( (x) => x.id === post.id);
+            postDetails.caption = post.caption;
+            console.log(postDetails);
+        });*/
     }
 
     render() {
         return (
-            <Header title="Image Viewer" positionLeft={<Search><Profile/></Search>}>
+            <Header title="Image Viewer" positionLeft={<Search><Profile /></Search>}>
+                <Box display="flex" width="100%" flexDirection="row" flexWrap="wrap" alignItems="center" justifyContent="spaceAround">
+                {
+                   postsDetail.map(post =>(
+                    <Card className="post">
+                    <PostHeader postUser={post.username} postedTime={post.timestamp}></PostHeader>
+                    <PostContent media={post.media_url} title={post.id} text={posts.data.find( (x) => x.id === post.id).caption} />
+                    <PostFooter postUser={post.username} likes={Math.round(100 + Math.random() * 100)}/>
+                </Card>
+                   ))
+                };
+                </Box>
             </Header>
         );
     }
